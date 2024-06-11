7d035774c6680ae217c9e151664bc13d1711213f
FilipeHibrael <filipehibrael27@gmail.com>
2024-06-11 20:49:58 -0300

    dark mode

--- src/components/header/header-dark-mode.tsx
+++ src/components/header/header-dark-mode.tsx
@@ -1,10 +1,23 @@
+'use client';
 import MoonIcon from '@/icons/moon-icon';
 import styles from './header-dark-mode.module.css';
+import React from 'react';
 
 export default function HeaderDarkMode() {
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  React.useEffect(() => {
+    document.body.classList.toggle('dark-mode');
+  }, [darkMode]);
+
   return (
-    <div className={styles.toggleContainer}>
-      <div className={styles.toggleButton}></div>
+    <div
+      className={`${styles.toggleContainer} ${darkMode ? styles.active : ''}`}
+    >
+      <div
+        className={styles.toggleButton}
+        onClick={() => setDarkMode(!darkMode)}
+      ></div>
       <MoonIcon />
     </div>
   );
