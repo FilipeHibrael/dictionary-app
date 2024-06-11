6c2e3e31451adb38fc345bcce8d7ac0d3f75ba6a
FilipeHibrael <filipehibrael27@gmail.com>
2024-06-11 20:05:53 -0300

    select font

--- src/components/header/header-select-font.tsx
+++ src/components/header/header-select-font.tsx
@@ -1,12 +1,45 @@
+'use client';
 import ArrowDownIcon from '@/icons/arrow-down-icon';
 import styles from './header-select-font.module.css';
 import React from 'react';
 
+const FONT_TYPES = ['sans-serif', 'serif', 'mono'];
+
+function formatString(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).replace('-', ' ');
+}
+
 export default function HeaderSelectFont() {
+  const [openMenu, setOpenMenu] = React.useState(false);
+  const [currentFont, setCurrentFont] = React.useState('sans-serif');
+
+  React.useEffect(() => {
+    FONT_TYPES.forEach((font) => document.body.classList.remove(font));
+
+    document.body.classList.add(currentFont);
+  }, [currentFont]);
+
   return (
-    <div className={styles.select}>
-      <p>Sans serif</p>
-      <ArrowDownIcon />
+    <div className={styles.selectContainer}>
+      <div
+        className={`${styles.select} ${openMenu ? styles.active : ''}`}
+        onClick={() => setOpenMenu(!openMenu)}
+      >
+        <p>{formatString(currentFont)}</p>
+        <ArrowDownIcon />
+      </div>
+
+      <ul className={`${styles.selectMenu} ${openMenu ? styles.active : ''}`}>
+        {FONT_TYPES.map((font) => (
+          <li
+            key={font}
+            className={`${currentFont === font ? styles.active : ''}`}
+            onClick={() => setCurrentFont(font)}
+          >
+            {formatString(font)}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
