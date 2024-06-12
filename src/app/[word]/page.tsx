2dcbc77646f11d791ff663821d52639badf2e903
FilipeHibrael <filipehibrael27@gmail.com>
2024-06-12 18:59:12 -0300

    word page

--- src/app/[word]/page.tsx
+++ src/app/[word]/page.tsx
@@ -1,11 +1,18 @@
 import Header from '@/components/header/header';
 import SearchBar from '@/components/search-bar/search-bar';
 
-export default function Home() {
+type WordParams = {
+  params: {
+    word: string;
+  };
+};
+
+export default function WordPage({ params }: WordParams) {
   return (
     <main className="container">
       <Header />
       <SearchBar />
+      <p>{params.word}</p>
     </main>
   );
 }
