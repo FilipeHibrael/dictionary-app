4d1089ca1d9edffafb062c5d0895a5a9df367a64
FilipeHibrael <filipehibrael27@gmail.com>
2024-06-12 19:47:42 -0300

    word href

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -1,9 +1,21 @@
+'use client';
 import SearchIcon from '@/icons/search-icon';
 import styles from './search-bar.module.css';
+import React, { FormEvent } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function SearchBar() {
+  const [word, setWord] = React.useState('');
+  const router = useRouter();
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formatWord = word.trim().replace(' ', '-').toLowerCase();
+    if (formatWord) router.push('/' + formatWord);
+  }
+
   return (
-    <form className={styles.form} action="">
+    <form className={styles.form} onSubmit={handleSubmit}>
       <label htmlFor="word" hidden>
         Word
       </label>
@@ -13,6 +25,8 @@ export default function SearchBar() {
         id="word"
         type="text"
         placeholder="Search for any word..."
+        required
+        onChange={(event) => setWord(event.target.value)}
       />
 
       <button className={styles.searchIcon} type="submit">
