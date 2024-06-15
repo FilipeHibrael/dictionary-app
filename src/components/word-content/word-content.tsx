3b7e61113ccf3a3bfc0f36a93687f25c99c08b5b
FilipeHibrael <filipehibrael27@gmail.com>
2024-06-15 00:22:31 -0300

    word content styles

--- src/components/word-content/word-content.tsx
+++ src/components/word-content/word-content.tsx
@@ -1,21 +1,29 @@
 import wordGet from '@/actions/word-get';
 import WordContentPhonetic from './word-content-phonetic';
 import WordContentMeaning from './word-content-meaning';
+import styles from './word-content.module.css';
+import NewWindowIcon from '@/icons/new-window-icon';
 
 export default async function WordContent({ word }: { word: string }) {
   const { data } = await wordGet(word);
 
   if (!data) return null;
   return (
-    <section>
+    <section className={styles.container}>
       <WordContentPhonetic word={data[0].word} phonetics={data[0].phonetics} />
 
       {data.map((wordData, i) => (
-        <div key={i}>
+        <div key={i} className={styles.meaningsContainer}>
           {wordData.meanings.map((meaning, i) => (
             <WordContentMeaning key={i} meaning={meaning} />
           ))}
-          <p>{wordData.sourceUrls[0]}</p>
+          <div className={`${styles.source} font-xs`}>
+            <span>Source</span>
+            <a target="_blank" href={wordData.sourceUrls[0]}>
+              {wordData.sourceUrls[0]}
+            </a>
+            <NewWindowIcon />
+          </div>
         </div>
       ))}
     </section>
